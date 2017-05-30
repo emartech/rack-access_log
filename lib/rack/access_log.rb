100c3019e7c9a3c6896d950da29e55efaf7eb629
Adam Luzsi <adamluzsi@gmail.com>
2017-05-30 13:11:47 +0200

    initial commit for gem

---
+++ lib/rack/access_log.rb
@@ -0,0 +1,7 @@
+require "rack/access_log/version"
+
+module Rack
+  module AccessLog
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/access_log/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module AccessLog
+    VERSION = "0.1.0"
+  end
+end
